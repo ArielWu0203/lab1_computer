45ae48a6816025e654a3c8d6e8ce9fbc839fcbae
ArielWu0203 <lucky020372@gmail.com>
2019-03-23 00:56:13 +0800

    TCP files transmit

--- lab1_file_transfer.c
+++ lab1_file_transfer.c
@@ -6,10 +6,18 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <sys/stat.h>
 
 void error (const char *msg) {
     perror(msg);
     exit;
+}
+
+int file_size(char* filename) {
+    struct stat statbuf;
+    stat(filename,&statbuf);
+    int size = statbuf.st_size;
+    return size;
 }
 
 int main (int argc,char *argv[]) {
@@ -38,6 +46,18 @@ int main (int argc,char *argv[]) {
     	}
         // listen socket <= 5
         listen(sockfd,5);
+
+        // get file size
+        int size = file_size(argv[5]);
+        printf("The size of file is %d\n",size);
+
+        // file open
+        FILE *fp;
+        fp = fopen(argv[5],"rb");
+        if(fp < 0) {
+            error("ERROR opening the file");
+        }
+
         // accept
         clilen = sizeof(cli_addr);
         newsockfd = accept(sockfd,(struct sockaddr*)&cli_addr,&clilen);
@@ -51,16 +71,38 @@ int main (int argc,char *argv[]) {
         if(n < 0) {
             error("ERROR reading from socket");
         }
-        printf("Here is the message : %s\n",buffer);
-        n = write(newsockfd,"I got your message",18);
+        // send file name
+        n = write(newsockfd,argv[5],sizeof(argv[5]));
         if(n < 0) {
             error("ERROR writing to socket");
         }
+        bzero(buffer,256);
+        // read from client
+        n = read(newsockfd,buffer,255);
+        if(n < 0) {
+            error("ERROR reading from socket");
+        }
+        printf("client : %s\n",buffer);
+       
+        // send file
+        int bytes_num;
+        int split_size = 200;
+        char buf[split_size];
+        while(!feof(fp)) {
+            bytes_num = fread(buf,sizeof(char),sizeof(buf),fp);
+            printf("READ %d bytes\n",bytes_num);
+            bytes_num = write(newsockfd,buf,bytes_num);
+            printf("SEND %d bytes\n",bytes_num);
+        }
+
         // close socket
+        fclose(fp);
         close(newsockfd);
         close(sockfd);
 
-    }else if(!strcmp(argv[1],"tcp") && !strcmp(argv[2],"recv")) {
+    }
+    
+    else if(!strcmp(argv[1],"tcp") && !strcmp(argv[2],"recv")) {
         
         int sockfd,portno,n;
         struct sockaddr_in serv_addr;
@@ -88,23 +130,54 @@ int main (int argc,char *argv[]) {
         if(connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) {
             error("ERROR connecting");
         }
+        
         // clear buffer
         bzero(buffer,256);
         strcpy(buffer,"client asks for files!");
+        // send
         n = write(sockfd,buffer,strlen(buffer));
         if(n < 0) {
             error("ERROR writing to socket");
         }
         bzero(buffer,256);
+        // recv
         n = read(sockfd,buffer,255);
         if(n < 0) {
             error("ERROR reading from socket");
         }
-        printf("%s\n",buffer);
+        
+        //file open
+        FILE *fp;
+        int bytes_num;
+        char buf[1000000];
+        char file_name[50] = "client/";
+        strcat(file_name,buffer);
+        
+        if ((fp = fopen(file_name,"wb")) == NULL ) {
+            error("ERROR opening the file");
+        }
+        bzero(buffer,256);
+        strcpy(buffer,"Start to send file to me!");
+        // send
+        n = write(sockfd,buffer,strlen(buffer));
+        if(n < 0) {
+            error("ERROR writing to socket");
+        }
+
+        while(1) {
+            bytes_num = read(sockfd,buf,sizeof(buf));
+            printf("READ %d bytes\n",bytes_num);
+            if(bytes_num == 0) {
+                break;
+            }
+            bytes_num = fwrite(buf,sizeof(char),bytes_num,fp);
+            printf("WRITE %d bytes\n",bytes_num);
+        }
+        
+        fclose(fp);
         close(sockfd);
 
     }
-    
     
     return 0;
 }
